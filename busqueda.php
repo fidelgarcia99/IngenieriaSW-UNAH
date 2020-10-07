7d42229e99ae503812ab5497348d464034c4cc0e
DianaBanegas <42158845+DianaBanegas@users.noreply.github.com>
2020-10-07 02:04:24 -0600

    Inventario

---
+++ busqueda.php
@@ -0,0 +1,15 @@
+<div class="field" id="searchform">
+     <input type="text" id="searchterm" placeholder="Producto" />
+     <button type="button" id="search">Buscar</button>
+</div>
+   <br>
+<label for="filter" >Elija un filtro:</label>
+                                        <select name="filter" id="filter" class="field">
+                                          <option value="marca" class="txt">Marca</option>
+                                          <option value="proveedor">Proveedor</option>
+                                          <option value="cantidad">Cantidad</option>
+                                          <option value="precio">Precio</option>
+                                        </select>
+   
+   <script class="cssdeck" src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+   <link rel="stylesheet" href="css/barrabusqueda.css">
