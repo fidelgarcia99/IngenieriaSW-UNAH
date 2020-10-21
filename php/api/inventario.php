5326a5447bc7676c9755e9e78efafeebd1527343
Luis Benitez <lfbenitez15@gmail.com>
2020-10-21 02:44:11 -0600

    Se modifico la consulta sql utilizada para mostrar los productos

--- php/api/inventario.php
+++ php/api/inventario.php
@@ -5,7 +5,7 @@
     require_once("../clases/class_conexion.php");
     require_once("../clases/class_jwt.php");
     require_once("../verificaToken.php");
-    $conexion = new Conexion();    
+    $conexion = new Conexion();
 
     session_start();
 
@@ -17,49 +17,34 @@
         break;
 
 
-        case 'GET':     //Obtener producto/s            
+        case 'GET':     //Obtener producto/s
             verificaToken($_GET['token']);
 
             if(isset($_GET['id'])){
 
             }else{
-               /* $resultado = $conexion->ejecutarInstruccion('SELECT 
-                p.idProducto,
-                p.nombre_producto,
-                p.fechaElaboracion,
-                p.fechaVencimiento,
-                p.precioCosto,
-                p.precioVenta,
-                p.precioOferta,
-                p.cantidad,
-                p.Contenedor_idContenedor,
-                prov.nombre_proveedor,
-                prov.telefono_proveedor,
-                cont.descripcion_cont,
-                pas.descripcion_pasillo,
-                a.nombre_area
+                $resultado = $conexion->ejecutarInstruccion('SELECT
+                p.idProducto as Id,
+                p.nombre_producto as Nombre,
+                p.precioCosto as Costo,
+                p.precioVenta as Precio,
+                p.cantidad as Cantidad,
+                prov.nombre_proveedor as Proveedor
             FROM
                 producto AS p
                     INNER JOIN
-                producto_x_proveedor pxp ON p.idProducto = pxp.Producto_idProducto
+                producto_x_proveedor  AS pxp
+                 ON p.idProducto = pxp.Producto_idProducto
                     INNER JOIN
-                proveedor prov ON pxp.Proveedor_idProveedor = prov.idProveedor
-                    INNER JOIN
-                contenedor cont ON p.Contenedor_idContenedor = cont.idContenedor
-                    INNER JOIN
-                pasillo_x_contenedor pxc ON cont.idContenedor = pxc.Contenedor_idContenedor
-                    INNER JOIN
-                pasillo pas ON pxc.Pasillo_idPasillo = pas.idPasillo
-                    INNER JOIN
-                area a ON pas.Area_idArea = a.idArea
-            GROUP BY p.idProducto HAVING COUNT(*) > 1');*/
-            $resultado = $conexion->ejecutarInstruccion('SELECT * FROM producto');
+                proveedor AS prov
+                 ON pxp.Proveedor_idProveedor = prov.idProveedor
+            ');
 
                 $res = array(); //creamos un array
-            
+
                 while($row = mysqli_fetch_assoc($resultado))
                 {
-                    $res[] = $row;                    
+                    $res[] = $row;
                 }
                 echo json_encode($res);
             }
