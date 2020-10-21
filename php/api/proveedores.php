041b4ec31929dd3d5a7caf4a41a98bb9260332e2
Luis Benitez <lfbenitez15@gmail.com>
2020-10-21 03:19:38 -0600

    se modifico la consulta de proveedores

--- php/api/proveedores.php
+++ php/api/proveedores.php
@@ -13,17 +13,33 @@
     //Servicios web
     switch($_SERVER['REQUEST_METHOD'])
     {
-        case 'POST':    
+        case 'POST':
             echo '{"res":"post"}';
         break;
 
-        case 'GET':     
+        case 'GET':
             verificaToken($_GET['token']);
 
             if(isset($_GET['id'])){
 
             }else{
-                $resultado = $conexion->ejecutarInstruccion('SELECT *  FROM proveedor');
+                $resultado = $conexion->ejecutarInstruccion('SELECT
+                p.idProveedor as Id,
+                p.nombre_proveedor as Nombre,
+                p.direccion_proveedor as Direccion,
+                p.telefono_proveedor as Telefono,
+                p.correo_proveedor as Email,
+                CONCAT(pe.pnombre," ", pe.papellido) as Representante,
+                t.num_telefono as Contacto
+            FROM
+                proveedor AS p
+                    INNER JOIN
+                persona  AS pe
+                 ON p.Persona_idPersona = pe.idPersona
+                    INNER JOIN
+                telefono AS t
+                 ON pe.idPersona = t.Persona_idPersona
+            ');
 
                 $res = array(); //creamos un array
 
