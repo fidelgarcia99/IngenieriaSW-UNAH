7e00145e0d7ee6235bb8e9cfaf6edece2480ce90
Luis Benitez <lfbenitez15@gmail.com>
2020-11-14 15:26:49 -0600

    listo el formulario para llantas, neumaticos y aceites de motor

--- js/inventario-controller.js
+++ js/inventario-controller.js
@@ -34,24 +34,55 @@ const cambioCategoria = function(){
     break;
     case 'Aceites':
     document.getElementById('div-detalles').innerHTML=`
-    <div class="form-label-group tercio">
-    Altura
-         <input type="number" pattern="[0-9]+" id="input-alto" class="form-control" placeholder="Altura" required>
+    <div class="form-label-group">
+    Tipo de Aceite
+      <div class="input-group">
+            <select class="form-control" id="select-tipo-aceite">
+              <option selected value="1">Sintetico</option>
+              <option value="2">Semi-sintetico</option>
+              <option value="3">Mineral</option>
+            </select>
+      </div>
     </div>
-    <div class="form-label-group tercio">
-    Ancho
-         <input type="number" pattern="[0-9]+" id="input-ancho" class="form-control" placeholder="Ancho" required>
+    <div class="form-label-group">
+    Viscosidad SAE
+      <div class="input-group">
+            <select class="form-control" id="select-tipo-aceite">
+              <option selected value="25W">25W</option>
+              <option selected value="20W">20W</option>
+              <option selected value="15W">15W</option>
+              <option selected value="10W">10W</option>
+              <option selected value="5W">5W</option>
+              <option selected value="0W">0W</option>
+            </select>
+            <select class="form-control" id="select-tipo-aceite">
+              <option selected value="50">50</option>
+              <option selected value="40">40</option>
+              <option selected value="30">30</option>
+              <option selected value="20">20</option>
+            </select>
+      </div>
     </div>
-    <div class="form-label-group tercio">
-    Diametro
-         <input type="number" pattern="[0-9]+" id="input-diametro" class="form-control" placeholder="Rin"required>
+    <div class="form-label-group">
+    Servicio API
+      <div class="input-group">
+            <select class="form-control" id="select-tipo-aceite">
+              <option selected value="SN">Motor a Gasolina (SN)</option>
+              <option selected value="SM">Motor a Gasolina (SM)</option>
+              <option selected value="SL">Motor a Gasolina (SL)</option>
+              <option selected value="CJ">Motor a Diesel (CJ)</option>
+              <option selected value="CI">Motor a Diesel (CI)</option>
+              <option selected value="CH">Motor a Diesel (CH)</option>
+            </select>
+      </div>
     </div>
     <div class="form-label-group">
     Tipo Vehiculo
       <div class="input-group">
             <select class="form-control" id="select-vehiculo">
-              <option selected value="1">Carro</option>
-              <option value="2">Moto</option>
+              <option selected value="0">Carro</option>
+              <option value="1">Moto 4T</option>
+              <option value="2">Moto 2T</option>
             </select>
       </div>
     </div>
@@ -63,60 +94,12 @@ const cambioCategoria = function(){
     break;
     case 'Lubricantes':
     document.getElementById('div-detalles').innerHTML=`
-    <div class="form-label-group tercio">
-    Altura
-         <input type="number" pattern="[0-9]+" id="input-alto" class="form-control" placeholder="Altura" required>
-    </div>
-    <div class="form-label-group tercio">
-    Ancho
-         <input type="number" pattern="[0-9]+" id="input-ancho" class="form-control" placeholder="Ancho" required>
-    </div>
-    <div class="form-label-group tercio">
-    Diametro
-         <input type="number" pattern="[0-9]+" id="input-diametro" class="form-control" placeholder="Rin"required>
-    </div>
-    <div class="form-label-group">
-    Tipo Vehiculo
-      <div class="input-group">
-            <select class="form-control" id="select-vehiculo">
-              <option selected value="1">Carro</option>
-              <option value="2">Moto</option>
-            </select>
-      </div>
-    </div>
-    <div class="form-label-group">
-    Observaciones
-         <input type="text" id="input-observacion" class="form-control" placeholder="Observaciones" required>
-    </div>
+
     `;
     break;
     case 'Consumible':
     document.getElementById('div-detalles').innerHTML=`
-    <div class="form-label-group tercio">
-    Altura
-         <input type="number" pattern="[0-9]+" id="input-alto" class="form-control" placeholder="Altura" required>
-    </div>
-    <div class="form-label-group tercio">
-    Ancho
-         <input type="number" pattern="[0-9]+" id="input-ancho" class="form-control" placeholder="Ancho" required>
-    </div>
-    <div class="form-label-group tercio">
-    Diametro
-         <input type="number" pattern="[0-9]+" id="input-diametro" class="form-control" placeholder="Rin"required>
-    </div>
-    <div class="form-label-group">
-    Tipo Vehiculo
-      <div class="input-group">
-            <select class="form-control" id="select-vehiculo">
-              <option selected value="1">Carro</option>
-              <option value="2">Moto</option>
-            </select>
-      </div>
-    </div>
-    <div class="form-label-group">
-    Observaciones
-         <input type="text" id="input-observacion" class="form-control" placeholder="Observaciones" required>
-    </div>
+
     `;
     break;
     case 'Neumaticos':
@@ -142,31 +125,7 @@ const cambioCategoria = function(){
     break;
     case 'Accesorios':
     document.getElementById('div-detalles').innerHTML=`
-    <div class="form-label-group tercio">
-    Altura
-         <input type="number" pattern="[0-9]+" id="input-alto" class="form-control" placeholder="Altura" required>
-    </div>
-    <div class="form-label-group tercio">
-    Ancho
-         <input type="number" pattern="[0-9]+" id="input-ancho" class="form-control" placeholder="Ancho" required>
-    </div>
-    <div class="form-label-group tercio">
-    Diametro
-         <input type="number" pattern="[0-9]+" id="input-diametro" class="form-control" placeholder="Rin"required>
-    </div>
-    <div class="form-label-group">
-    Tipo Vehiculo
-      <div class="input-group">
-            <select class="form-control" id="select-vehiculo">
-              <option selected value="1">Carro</option>
-              <option value="2">Moto</option>
-            </select>
-      </div>
-    </div>
-    <div class="form-label-group">
-    Observaciones
-         <input type="text" id="input-observacion" class="form-control" placeholder="Observaciones" required>
-    </div>
+    
     `;
     break;
   }
